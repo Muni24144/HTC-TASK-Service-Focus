cd6824f0d30dd28775e4c03fccbfafdaf999c239
Muni Kumar Jakkula <munikumar.jakkula@htcindia.local>
2021-12-17 17:04:14 +0530

    completed task of service focus

---
+++ script.js
@@ -0,0 +1,14 @@
+function menu(id){
+    document.getElementById("nav").classList.toggle("nav_active")
+    document.getElementById("i1").classList.toggle("i1_none")
+    document.getElementById("i2").classList.toggle("i2_active")
+}
+function menu2(id){
+  document.getElementById("nav").classList.toggle("nav_active")
+  document.getElementById("i1").classList.toggle("i1_none")
+  document.getElementById("i2").classList.toggle("i2_active")
+}
+
+
+
+
