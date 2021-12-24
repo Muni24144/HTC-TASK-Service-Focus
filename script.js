c27c7183d0f263f97f3127bf5b697d5360e213d1
Muni Kumar Jakkula <munikumar.jakkula@htcindia.local>
2021-12-24 11:51:51 +0530

    fixed minor issues

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function menu(id){
+function menu(){
     document.getElementById("nav").classList.toggle("nav_active")
     document.getElementById("i1").classList.toggle("i1_none")
     document.getElementById("i2").classList.toggle("i2_active")
 }
-function menu2(id){
+function menu2(){
   document.getElementById("nav").classList.toggle("nav_active")
   document.getElementById("i1").classList.toggle("i1_none")
   document.getElementById("i2").classList.toggle("i2_active")
